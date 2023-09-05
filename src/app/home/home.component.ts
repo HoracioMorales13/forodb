b733ce203b3de36fbd237f33b5125d3d95f74eff
HoracioMorales13 <142364230+HoracioMorales13@users.noreply.github.com>
2023-09-05 11:26:20 -0600

    editar login

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  preguntas = [
+    {id: 1, pregunta:'¿Cuál es la capital de Francia?'},
+    {id: 2, pregunta:'¿Cuál es la capital de España?'},
+    {id: 2, pregunta:'¿Cuál es la capital de Brasil?'},
+    {id: 2, pregunta:'¿Cuál es la capital de México?'},
+  ]
+  
+}
