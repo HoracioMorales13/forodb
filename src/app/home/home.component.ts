54a6226e9b915fbb5583cc0c866fec08e2b84be4
HoracioMorales13 <142364230+HoracioMorales13@users.noreply.github.com>
2023-09-06 09:47:02 -0600

    06/09/2023

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiRestService } from '../api-rest.service';
 
 @Component({
   selector: 'app-home',
@@ -13,4 +14,18 @@ export class HomeComponent {
     {id: 2, pregunta:'¿Cuál es la capital de México?'},
   ]
   
+  constructor(private api: ApiRestService){}
+
+  ngOnInit():void{
+    this.consulta()
+  }
+
+  consulta(){
+    this.api.getAllpreguntas().subscribe({
+      next: datos => {
+        console.log(datos)
+      },
+      error: e => {}
+    })
+  }
 }
